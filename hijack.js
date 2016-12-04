f6b37c938a78a3720401b82c924cdf51158de78e
Animesh Shrivastav <omglivekitchen@gmail.com>
2016-12-04 22:17:06 +0800

    First Commit

---
+++ hijack.js
@@ -0,0 +1,42 @@
+console.log("entered Hijack Java Script...");
+
+$(document).ready
+(	
+		function()
+		{console.log("executing loadpage() 1st time...");
+
+			loadPage();
+		}
+);
+
+function loadPage(url) 
+{
+	if(url==undefined)
+	{
+		url="index.html"
+		$("html").load(url + ' div',hijack);
+		console.log("loading()...url="+url+ " its body");
+	}
+	else 
+	{
+		$("body").load(url + ' article', hijack);
+		console.log("loading()...url="+url+" its article");
+	}
+	
+	
+}
+
+
+function hijack()
+{
+  $('a').click(
+	function(e)
+	  {
+		e.preventDefault();
+		loadPage(e.target.href);
+		console.log("Hijacking...href="+e.target.href);
+
+	  }
+		    );
+}
+
